b11c430e081fc3cf5da764c58f0c9d39e40d33b3
GuilFranca <guilfranca27@gmail.com>
2025-04-24 21:55:38 -0300

    finalizando aula 24

--- Aulas/Aula24_04_25/exe2.js
+++ Aulas/Aula24_04_25/exe2.js
@@ -12,6 +12,13 @@ for (let i = 1930; i <= 2025; i++) {
 
 }
 
-let numeroAnoCopa = prompt('Digite qual copa do mundo você quer saber: ');
+// let numeroAnoCopa = prompt('Digite qual copa do mundo você quer saber: ');
 
-console.log(anoCopa[numeroAnoCopa - 1]);
+// console.log(anoCopa[numeroAnoCopa - 1]);
+
+let ultimoAno = anoCopa.length - 1;
+
+console.log(`O quarto ano de copa do mundo ${anoCopa[3]}`);
+console.log(`O decimo ano de copa do mundo ${anoCopa[9]}`);
+console.log(`O ultimo ano de copa do mundo ${anoCopa[ultimoAno]}`);
+console.log(`O ultimo ano de copa do mundo ${anoCopa[0]}`);
