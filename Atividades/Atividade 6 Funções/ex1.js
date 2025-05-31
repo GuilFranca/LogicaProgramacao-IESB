1c87d521f9967c4abefd809f815693383eed3d2e
Guilherme França Fernandes <guilfranca27@gmail.com>
2025-05-30 23:20:57 -0300

    código do demonio

--- Atividades/Atividade 6 Funções/ex1.js
+++ Atividades/Atividade 6 Funções/ex1.js
@@ -1,4 +1,4 @@
-//1. Crie uma função em JS que retorne a área de um trapézio,
+// 1. Crie uma função em JS que retorne a área de um trapézio,
 // cuja altura mede h e cujas bases medem B e b.
 // At= (B+b)*h/2
 
@@ -6,7 +6,7 @@ const prompt = require('prompt-sync')({signint: true});
 
 function calculoTrapezio(altura, baseMaior, baseMenor) {
 
-    let area = ((baseMaior + baseMenor) * altura) / 2; 
+    let area = ((baseMaior + baseMenor) * altura) / 2;
 
     return area;
 
