f7cbf64d8b4cba6220a1bbf78f3092f1296a556f
Guilherme França Fernandes <guilfranca27@gmail.com>
2025-06-30 22:18:31 -0300

    revisão listas e poo

---
+++ revisão/quest1.js
@@ -0,0 +1,24 @@
+let a = [0, 1, 2];
+let b = [3, 4, 5];
+
+// nessa linha será exibido o 2 e depois ele será apagado da lista
+console.log(a.pop());
+
+// Vai mostrar a lista A completa
+console.log(a);
+
+// Vai mostrar a lista B completa
+console.log(b);
+
+// nessa linha será exibido a quantidade de elementos que há na array após a inserção do 6 no fim da lista
+console.log(b.push(6));
+// console.log(b.push(5));
+// console.log(b.push(7));
+
+// Vai mostrar a lista B completa
+console.log(b);
+
+// Adiciona o -1 no início do array e mosta quantos elementos tem na array
+console.log(a.unshift(-1));
+
+console.log(a);
---
+++ revisão/quest2.js
@@ -0,0 +1,64 @@
+// Herança é a habilidade de possuir caracteristicas do objeto pai, sendo assim uma forma de herança. Podendo herdar atributos, constructors, getters, setter ou métodos.
+
+// Polimorfismo é quando um objeto pode se comportar de várias formas, podendo ter diferentes funcionalidades para um mesmo método.
+
+// Encapsulamento é uma forma de proteger a aplicação privando os atributos, fazendo com que os mesmos só possam ser editados ou utilizados por meio de métodos.
+
+class Conta {
+
+    #saldo;
+
+    constructor(saldoInicial) {
+        this.#saldo = saldoInicial;
+    }
+
+    depositar(valor) {
+
+        if (valor > 0) {
+            this.#saldo += valor;
+        } else {
+            console.log('Valor inválido!')
+        }
+    }
+
+    sacar(valor) {
+        this.#saldo -= valor;
+    }
+
+    getSaldo() {
+        return this.#saldo;
+    }
+
+    setSaldo(valor) {
+        this.#saldo = valor;
+    }
+
+}
+
+class ContaNome extends Conta {
+
+    #nome;
+
+    constructor(saldoInicial, nome) {
+        // Chama o construtor da classe pai Conta
+        super(saldoInicial)
+        this.#nome = nome;
+    }
+
+    getNome() {
+        return this.#nome;
+    }
+
+    setNome(nome) {
+        this.#nome = nome;
+    }
+
+}
+
+const nubank = new Conta(1000);
+
+console.log(`Saldo inicial da Nubank: R$${nubank.getSaldo()}`);
+
+const inter = new ContaNome(5000, 'Guilherme')
+
+console.log(`Saldo inicial de ${inter.getNome()} Nubank: R$${inter.getSaldo()}`);
